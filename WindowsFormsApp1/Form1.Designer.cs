dc49b431bfebec0cdb6979a4e86e79df065757c7
Hengshuofushi <huqiang0705@outlook.com>
2022-05-04 14:57:26 +0800

    commit ms_parser

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.videoFrameCountTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Location = new System.Drawing.Point(12, 12);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(94, 23);
+            this.chooseFileButton.TabIndex = 0;
+            this.chooseFileButton.Text = "choose file";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.chooseFileButton_Click);
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(136, 12);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(642, 21);
+            this.fileNameTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Video Frame Count:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.videoFrameCountTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(763, 205);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PS Stream Attribute";
+            // 
+            // videoFrameCountTextBox
+            // 
+            this.videoFrameCountTextBox.Location = new System.Drawing.Point(125, 14);
+            this.videoFrameCountTextBox.Name = "videoFrameCountTextBox";
+            this.videoFrameCountTextBox.Size = new System.Drawing.Size(100, 21);
+            this.videoFrameCountTextBox.TabIndex = 3;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.chooseFileButton);
+            this.Name = "MainWindow";
+            this.Text = "Media Stream Parser";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button chooseFileButton;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox videoFrameCountTextBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.AddExtension = true;
+            dialog.RestoreDirectory = true;
+            DialogResult dialogResult = dialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                string filePath = dialog.FileName;
+                string fileName = filePath.Substring(filePath.LastIndexOf("\\") + 1);
+                fileNameTextBox.Text = filePath;
+                FileStream fs = (FileStream)dialog.OpenFile();
+                parseMediaStream(fs);
+            }
+        }
+
+        private void parseMediaStream(FileStream fs)
+        {
+            // TODO: parse media stream
+        }
+    }
+}
